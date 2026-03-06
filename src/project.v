04c0252476df9b16fa2e7749e618aca810853338
zetcord <157110192+zetcord@users.noreply.github.com>
2026-03-06 12:03:14 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_alu4bit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,31 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uio_out = 0; // We are not using this as output
+    assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+    wire _unused = &{ena,uio_in[2],uio_in[3],uio_in[4],uio_in[5],uio_in[6],uio_in[7],1'b0};
+    //alu logic
+    reg[3:0] result;
+    wire[3:0] a,b;
+    assign a=ui_in[3:0];
+    assign b=ui_in[7:4];
+    wire[1:0] op;
+    assign op=uio_in[1:0];
+    
+    always @(posedge clk) begin
+    if(!rst_n)
+        result<=0;
+    else
+        case(op)
+            2'b00: result<=a+b;
+            2'b01: result<=a-b;
+            2'b10: result<=a&b;
+            2'b11: result<=a|b;
+        endcase
+    end
+    assign uo_out[3:0]=result;//assign result
+    
 endmodule
